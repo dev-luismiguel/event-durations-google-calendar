40e9edba598ad34827e5e07c6987e2d53edb5604
Luis Miguel <lmigueldev@gmail.com>
2025-08-05 13:33:41 -0300

    Fix error in background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 function openOptionsPage() {
   var optionsPath = 'options/options.html';
-  var optionsUrl = chrome.extension.getURL(optionsPath);
-  chrome.tabs.create({url: optionsPath});
+  var optionsUrl = chrome.runtime.getURL(optionsPath);
+  chrome.tabs.create({url: optionsUrl});
 }
 
 chrome.action.onClicked.addListener(function(tab) {
